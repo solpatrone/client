e8c60c20019359c418861bcb290ec6fb3b580a2a
FlorenciaPD <florperezduarte@hotmail.com.ar>
2022-02-02 14:54:16 -0300

    changes logo

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/restaurante.png";
 import "./Navbar.css";
 
 export default function Navbar() {
