2ea4f44f4be92dd9c0a13fdb62b71a3242262d01
Florencia Perez Duarte <87536349+FlorPD@users.noreply.github.com>
2022-02-02 16:07:15 -0300

    Feat/nav bar (#12)
    * NavBar with mockup logo, links to register and to login (ugly css)
    
    * changes logo
    
    * changes logo
    
    * changes logo

---
+++ src/components/LandingPage/Landingpage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import SearchBar from "../SearchBar/Searchbar";
+
+function Landingpage() {
+  return (
+    <div>
+    <div>
+    <h3>Encontrá el mejor Restaurant en CABA y reservá tu mesa!</h3>
+      <SearchBar />
+    </div>
+    <img/>
+    
+
+    </div>
+  );
+}
+
+export default Landingpage;
