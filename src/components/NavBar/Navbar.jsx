fd28f7bfa78af93bc3208a7a645b019bd0a26525
FlorenciaPD <florperezduarte@hotmail.com.ar>
2022-02-02 13:57:46 -0300

    NavBar with mockup logo, links to register and to login (ugly css)

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div class="navBar">
+      <div class="links">
+        <NavLink to="/loginrestaurant">
+          <p>Mi Restaurant</p>
+        </NavLink>
+        <NavLink to="/registeruser">
+          <p>Registrar Restaurant</p>
+        </NavLink>
+      </div>
+
+      <div class="mainNavbar">
+        <NavLink to="/home">
+          <img class="logo" src={logo} alt="img not found" />
+        </NavLink>
+        <div class="links">
+          <NavLink to="/login">
+            <p>Iniciar sesión</p>
+          </NavLink>
+          <NavLink to="/registerclient">
+            <p>Crear cuenta</p>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+}
