6d551b2b41aee582efa8c9de19ce97b3864d3e10
solpatrone <sol.patrone@hotmail.com>
2022-02-03 12:23:51 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,34 +3,17 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Registerclient from "./components/RegisterClient/Registerclient";
 import RegisterOwner from "./components/RegisterOwner/Registerowner";
-<<<<<<< HEAD
 import Home from "./components/Home/Home";
-=======
-import Home from './components/Home/Home'
 import Login from "./components/Login/Login";
-
-
->>>>>>> 239d4851ec72f5e25ea6d827b7b0f04008f623da
 
 function App() {
   // const location = useLocation();
   return (
-<<<<<<< HEAD
     <div className="App">
       <Route path={"/home"} render={() => <Home />} />
       <Route path={"/registerclient"} render={() => <Registerclient />} />
       <Route path={"/registerOwner"} render={() => <RegisterOwner />} />
-=======
-    
-     <div className="App">
-
-       <Route path={'/home'} render={() => <Home/>} />
-       <Route path={'/registerclient'} render={() => <Registerclient/>} />
-       <Route path={'/registerOwner'} render={() => <RegisterOwner/>} />
-       <Route path={'/Login'} render={() => <Login/>} />
-       
-      
->>>>>>> 239d4851ec72f5e25ea6d827b7b0f04008f623da
+      <Route path={"/Login"} render={() => <Login />} />
     </div>
   );
 }
