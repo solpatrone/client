907c7fca8bf05f226be33b4d17a31cb859c18215
erikamasini1 <91484157+erikamasini1@users.noreply.github.com>
2022-02-03 12:16:53 -0300

    new merge

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,11 +11,7 @@ export default function Home(){
 
     return (
         <div>
-            <h1>Esta es la home!</h1>
-            <div>
-            <NavLink activeStyle={{ backgroundColor: '#BFA2DB' }}  to={'/registerclient'}>|Cliente|   </NavLink>
-            <NavLink activeStyle={{ backgroundColor: '#BFA2DB' }}  to={'/registerOwner'}>|Dueño|</NavLink>
-             </div>
+
             <Navbar/>
             <Landingpage/>
           
