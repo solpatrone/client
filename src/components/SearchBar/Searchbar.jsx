605fc3f363b29727cb7e2a647f02c7951ad0d01e
FlorenciaPD <florperezduarte@hotmail.com.ar>
2022-02-03 00:33:11 -0300

    create landing page with navBar and autocomplete searchBar

--- src/components/SearchBar/Searchbar.jsx
+++ src/components/SearchBar/Searchbar.jsx
@@ -3,18 +3,41 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 
 function Searchbar() {
+  const resto = ["bilbao", "la dominga", "jhonny b good", "bilbao2"];
   const dispatch = useDispatch();
   const [restaurantName, setRestaurantName] = useState("");
+  const [suggestion, setSuggestion] = useState([]);
+  const [showOptions, setShowOptions] = useState(false);
 
   function handleInputChange(e) {
     e.preventDefault();
     setRestaurantName(e.target.value);
+    searchMatch(e.target.value);
   }
   function handleSubmit(e) {
     e.preventDefault();
     dispatch(); // action getRestaurant(restaurantName)
     setRestaurantName("");
   }
+
+  function searchMatch(restaurantName) {
+    if (!restaurantName) {
+      setSuggestion([]);
+      setShowOptions(false);
+    } else {
+      let matches = resto.filter(
+        (r) => r.toLowerCase().indexOf(restaurantName.toLocaleLowerCase()) > -1
+      );
+      if (matches) {
+        setShowOptions(true);
+        setSuggestion(matches);
+      }
+    }
+  }
+  const selectElementHandler = (restaurant) => {
+    setRestaurantName(restaurant);
+    setShowOptions(false);
+  };
 
   return (
     <div>
@@ -24,6 +47,15 @@ function Searchbar() {
         placeholder="Nombre del Restaurant..."
         onChange={(e) => handleInputChange(e)}
       />
+      {suggestion &&
+        showOptions &&
+        suggestion.map((el, index) => {
+          return (
+            <div key={index} onClick={() => selectElementHandler(el)}>
+              {el}
+            </div>
+          );
+        })}
       <button type="submit" onClick={(e) => handleSubmit(e)}>
         Buscar
       </button>
