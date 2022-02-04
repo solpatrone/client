bebfb66c4f4096dc16bb0022347af858ed21ca19
FlorenciaPD <florperezduarte@hotmail.com.ar>
2022-02-04 16:25:35 -0300

    details in progress

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {getRestoDetails}  from '../../actions'
+import {useDispatch, useSelector } from "react-redux";
+import {Link, useParams } from "react-router-dom";
+
 
 function Details() {
+const dispatch = useDispatch()
+const params = useParams()
+const restaurants = useSelector((state) => state.restaurants)
+console.log(restaurants)
+console.log(params.id)
+useEffect(() => {
+  dispatch(getRestoDetails(params.id));
+}, [dispatch, params.id]);
+
 
   return(
     <div>
-        holaa
+        {
+            restaurants.lenght > 0 ? 
+            <div>
+                <div>
+                    <h2>{restaurants[0].name}</h2>
+                </div>
+            </div>
+              :
+              <h3>Los detalles del restaurant no estan disponibles</h3>
+        }
+      
     </div>
   )
    
