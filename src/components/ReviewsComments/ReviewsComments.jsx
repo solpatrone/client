e0d87cc429209cc702d3ded5943f249bf27fe933
FlorenciaPD <florperezduarte@hotmail.com.ar>
2022-02-07 18:18:55 -0300

    show reviews

--- src/components/ReviewsComments/ReviewsComments.jsx
+++ src/components/ReviewsComments/ReviewsComments.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import {useSelector} from 'react-redux'
+import {RiStarFill }from 'react-icons/ri'
+import s from './ReviewsComments.module.css'
+
 export default function ReviewsComments(){
+
+    const reviews = useSelector((state) => state.reviews);
+    console.log(reviews)
+
     return(
-        <div>Aca van a ir las reviews..</div>
+         reviews && reviews.map(r =>{
+             return(
+                <div className={s.review}>
+               <p>{[...Array(Number(r.rating.value)).keys()].map(() => (<RiStarFill />))}</p>
+                <p>{r.rev}</p>
+            </div>
+            
+             )
+             
+            
+         })
+        
     )
 }
