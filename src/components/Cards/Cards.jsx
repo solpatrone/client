ddc711631d482846d11191ab74a7b1cb1fbae7cd
solpatrone <sol.patrone@hotmail.com>
2022-02-08 11:16:41 -0300

    pre commit changes

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { useSelector } from 'react-redux';
 import Card from "../Card/card";
 import { Link } from "react-router-dom";
-import Loading from "../Loading/Loading";
 import style from "./Cards.module.css";
-import { LOADING } from "../../actions/types";
 export default function Cards({ restaurants }) {
-  const loading = useSelector((state) => state.loading);
   return (
-    <div >
-
-        {(restaurants.length > 0) ? 
-        
-          <section className={style.slider}>
-
+    <div>
+      {restaurants.length > 0 ? (
+        <section className={style.slider}>
           {restaurants.map((r) => {
             return (
-              <Link to={`/restaurants/${r.id}`} className={style.linkCard}>
+              <Link
+                to={`/restaurants/${r.id}`}
+                className={style.linkCard}
+                key={r.id}
+              >
                 <div className={style.box} key={r.id}>
                   <Card
                     name={r.name}
@@ -28,9 +25,10 @@ export default function Cards({ restaurants }) {
               </Link>
             );
           })}
-          </section>
-        
-          : <div className={style.errors}>No se encontraron restaurantes</div>}
+        </section>
+      ) : (
+        <div className={style.errors}>No se encontraron restaurantes</div>
+      )}
     </div>
   );
 }
---
+++ src/components/Reservation/Reservations.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Reservations() {
+  return <h1>Reservas</h1>;
+}
