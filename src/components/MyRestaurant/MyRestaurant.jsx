cee4bc81975549847f26431933639c118ac8f7a3
solpatrone <sol.patrone@hotmail.com>
2022-02-14 10:37:10 -0300

    styles

--- src/components/MyRestaurant/MyRestaurant.jsx
+++ src/components/MyRestaurant/MyRestaurant.jsx
@@ -13,10 +13,12 @@ import Review from "../Review/Review";
 import Loading from "../Loading/Loading";
 import styles from "./MyRestaurant.module.css";
 import { Widget } from "@uploadcare/react-widget";
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from "react-bootstrap/Carousel";
 import { addImagesToRestos, getRestoReservations } from "../../actions";
 import RestoReservations from "../RestoReservations/RestoReservations";
-
+import { Tab, Row, Col, Nav } from "react-bootstrap";
+import { AiOutlineDelete } from "react-icons/ai";
+import { FcCheckmark } from "react-icons/fc";
 
 export default function Restaurant() {
   const dispatch = useDispatch();
@@ -39,144 +41,193 @@ export default function Restaurant() {
   }, [params.id]);
 
   function handleChange(e) {
-    var photo = []
-    
-    for (let index = 0; index < e.count ; index++) {
-     photo.push(('https://ucarecdn.com/' + e.uuid + '/nth/' + index + '/').toString())          
-     setPhoto(photo)
+    var photo = [];
+
+    for (let index = 0; index < e.count; index++) {
+      photo.push(
+        ("https://ucarecdn.com/" + e.uuid + "/nth/" + index + "/").toString()
+      );
+      setPhoto(photo);
     }
   }
 
   function handleClick(e) {
-    e.preventDefault()
+    e.preventDefault();
     const request = {
       owner: myRestaurant.owner,
-      photo: photo
-    }
-    dispatch(addImagesToRestos(request, myRestaurant.id))
-    dispatch(getRestoDetails(params.id))
+      photo: photo,
+    };
+    dispatch(addImagesToRestos(request, myRestaurant.id));
+    dispatch(getRestoDetails(params.id));
 
-   
     //window.location.reload(false);
-   
   }
 
   return (
     <div>
       <Navbar />
-      {!myRestaurant.id? (
+
+      {!myRestaurant.id ? (
         <Loading />
       ) : (
-        <div className={styles.wrapper}>
-          <div className={styles.container}>
-            <div className={styles.restaurantInfo}>
-              <h2>{myRestaurant.name}</h2>
+        <div className={styles.centeredContainer}>
+          <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+            <Row>
+              <Col sm={3}>
+                <Nav variant="pills" className="flex-column">
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Mi Restaurante</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Reservas</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">Reseñas</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+              <Col sm={9}>
+                <Tab.Content>
+                  <Tab.Pane eventKey="first">
+                    <div className={styles.restaurantInfo}>
+                      <div className={styles.tittle}>
+                        <h2 className={styles.restoName}>
+                          {myRestaurant.name}
+                        </h2>
 
-              <div className={styles.address_icons}>
-                <div className={styles.address}>
-                  <p>
-                    Direccion:
-                    {myRestaurant.address.split(",", 1) +
-                      ", " +
-                      myRestaurant.neighborhood_info[0]}
-                  </p>
-                  {myRestaurant.email !== " - " && (
-                    <p>
-                      Contacto:
-                      {" " + myRestaurant.email}
-                    </p>
-                  )}
-                </div>
-                <div className={styles.icons}>
-                  <p>
-                    {[...Array(Number(myRestaurant.rating)).keys()].map(
-                      (key) => (
-                        <RiStarFill
-                          size={20}
-                          style={{ fill: "#f2d349" }}
-                          key={key}
+                        <AiOutlineDelete
+                          style={{
+                            color: "var(--error-color)",
+                            fontSize: "25px",
+                          }}
                         />
-                      )
-                    )}
-                  </p>
-                  {myRestaurant.price && (
-                    <p>
-                      {[...myRestaurant.price.split("")].map(() => (
-                        <BsCurrencyDollar size={20} />
-                      ))}
-                    </p>)}
+                      </div>
+                      <div className={styles.address_icons}>
+                        <div className={styles.address}>
+                          <p>
+                            Dirección:
+                            {myRestaurant.address.split(",", 1) +
+                              ", " +
+                              myRestaurant.neighborhood_info[0]}
+                          </p>
+                          {myRestaurant.email !== " - " && (
+                            <p>
+                              Contacto:
+                              {" " + myRestaurant.email}
+                            </p>
+                          )}
+                        </div>
+                        <div className={styles.icons}>
+                          <p>
+                            {[...Array(Number(myRestaurant.rating)).keys()].map(
+                              (key) => (
+                                <RiStarFill
+                                  size={13}
+                                  style={{ fill: "#f2d349" }}
+                                  key={key}
+                                />
+                              )
+                            )}
+                          </p>
+                          {myRestaurant.price && (
+                            <p>
+                              {[...myRestaurant.price.split("")].map(() => (
+                                <BsCurrencyDollar
+                                  size={13}
+                                  style={{ color: "var(--dark-color)" }}
+                                />
+                              ))}
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                      <Carousel className={styles.restauranteImage}>
+                        {myRestaurant &&
+                          myRestaurant.photo.map((el, index) => {
+                            return (
+                              <Carousel.Item
+                                key={index}
+                                className={styles.itemC}
+                              >
+                                <img
+                                  className="d-block w-100 h-100"
+                                  src={el}
+                                  alt="First slide"
+                                />
+                              </Carousel.Item>
+                            );
+                          })}
+                      </Carousel>
 
-                </div>
-              </div>
+                      <div className="mt-3">
+                        <Widget
+                          ref={widgetApi}
+                          publicKey="0a91ec69631fd28d2d4a"
+                          multiple="true"
+                          imagesOnly="true"
+                          locale="es"
+                          onChange={handleChange}
+                        />
+                        <div>
+                          {photo && (
+                            <button
+                              onClick={(e) => handleClick(e)}
+                              className={styles.button}
+                            >
+                              Guardar Cambios
+                            </button>
+                          )}
+                        </div>
+                      </div>
+                      <span>
+                        {myRestaurant.cuisine.map((el, index) => (
+                          <div key={index}>
+                            <FcCheckmark /> {el}
+                          </div>
+                        ))}
+                      </span>
+                      {myRestaurant.description && (
+                        <p className={styles.description}>
+                          {myRestaurant.description}
+                        </p>
+                      )}
+                    </div>
+                  </Tab.Pane>
 
+                  <Tab.Pane eventKey="second">
+                    <div>
+                      <h3>Reservas</h3>
 
-            
-                <Carousel className={styles.restauranteImage}>
- {myRestaurant && myRestaurant.photo.map((el, index) => {return ( 
- <Carousel.Item key={index}>
-    <img
-      className="d-block w-100"
-      src={el}
-      alt="First slide"
-    />
-  </Carousel.Item>)})}
- 
-</Carousel>
-
-
-
-
-              <div className="mt-3">
-                <Widget ref={widgetApi} publicKey='0a91ec69631fd28d2d4a' multiple='true' imagesOnly='true' locale='es' onChange={handleChange} />
-                <div>{photo &&
-                  <button onClick={e => handleClick(e)}>
-                    Guardar Cambios
-                  </button>
-                }
-             </div>
-      </div>
-              <span>
-                {myRestaurant.cuisine.map((el, index) => (
-                  <div key={index} className={styles.tag}>{el}</div>
-                ))}
-              </span>
-              {myRestaurant.description && (
-                <p className={styles.description}>
-                  {myRestaurant.description}
-                </p>
-              )}
-            </div>
-          </div>
-          <div>
-            {hasReviews.length > 0 && (
-              <div className={styles.reviews}>
-                <h3>Opiniones</h3>
-                <Review reviews={hasReviews} />
-              </div>
-            )}
-          </div>
-
-          <div>
-            <h3>Reservas</h3>
-
-            {myReservations.length > 0
-              ? myReservations.map((r) => (
-                  <RestoReservations
-                    id={r.id}
-                    key={r.id}
-                    pax={r.pax}
-                    date={r.date}
-                    time={r.time}
-                    username={r.author}
-                  />
-                ))
-              : "Aun no hay reservas"}
-          </div>
+                      {myReservations.length > 0
+                        ? myReservations.map((r) => (
+                            <RestoReservations
+                              id={r.id}
+                              key={r.id}
+                              pax={r.pax}
+                              date={r.date}
+                              time={r.time}
+                              username={r.author}
+                            />
+                          ))
+                        : "Aun no hay reservas"}
+                    </div>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="third">
+                    <div>
+                      {hasReviews.length > 0 && (
+                        <div className={styles.reviews}>
+                          <h3>Opiniones</h3>
+                          <Review reviews={hasReviews} />
+                        </div>
+                      )}
+                    </div>
+                  </Tab.Pane>
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
         </div>
       )}
-
-      
     </div>
   );
 }
-
