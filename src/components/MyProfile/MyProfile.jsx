87e5bf7f00f3b10409387b322f4eec2f730f00b8
Christian Hampel <80498051+chrishampel88@users.noreply.github.com>
2022-02-16 13:09:09 -0300

    styleProfile

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -32,7 +32,6 @@ export default function MyProfile() {
     <div>
       <Navbar />
       <div className={s.centeredContainer}>
-      <h1>Mi perfil </h1>
         <Tab.Container id="left-tabs-example" defaultActiveKey="first" >
           <Row>
           <Col sm={3}>
@@ -51,7 +50,7 @@ export default function MyProfile() {
             <Col sm={9}>
               <Tab.Content>
                 <Tab.Pane eventKey="first">
-                  <h2>Mis reservas</h2>
+              <div className={s.reviewsContainer}>
                       {
                         reservesInProgress  ?
                         reservesInProgress.map( (e, index) =>
@@ -62,10 +61,11 @@ export default function MyProfile() {
                                 <p>No hay reservas</p>
                             </div>
                       }        
+                      </div>
                 </Tab.Pane >
 
                 <Tab.Pane eventKey="second">
-                  <h2>Mis reseñas</h2>
+                <div className={s.reservationsContainer}>
                       {
                         reviews.length > 0 ?
                         reviews.map( (e, index) =>
@@ -76,10 +76,10 @@ export default function MyProfile() {
                               <p>No hay reviews</p>
                           </div>
                       }
+                      </div>
                 </Tab.Pane>
 
                 <Tab.Pane eventKey="third">
-                <h2>Mis favoritos</h2>
                 {
           
                 }
