d41b149f6f6f38afb0216bac50717e2bce8e7b80
Christian Hampel <80498051+chrishampel88@users.noreply.github.com>
2022-02-17 08:55:19 -0300

    responsiveHome

--- src/components/UserReserve/UseReserve.jsx
+++ src/components/UserReserve/UseReserve.jsx
@@ -14,14 +14,7 @@ export default function UserReserve(props) {
 
                 </div>
                 <div className={s.inputContainer}>
-                    {/*<div className={s.input}>
-                        <p>
-                            <FaUserCircle size={30} style={{ fill: "var(--medium-color)" }} />
-                        </p>
-                    <span>
-                        {elem.author}
-                    </span>
-                    </div>*/}
+             
                 <div className={s.input}>
                     <p>
                         <FaRegCalendarAlt style={{ color: "var(--bright-color)" }} /> Fecha
@@ -50,39 +43,3 @@ export default function UserReserve(props) {
         </div> 
     )
 }
-//return (
-//    <div className={s.container}>
-//      <div className={s.header}>
-
-//        <div>
-//          <FaUserCircle size={30} style={{ fill: "var(--medium-color)" }} />
-//        </div>
-//        <div>{username}</div>
-//      </div>
-
-//      <div className={s.inputContainer}>
-//        <div className={s.input}>
-//          <p>
-//            <FaRegCalendarAlt style={{ color: "var(--bright-color)" }} /> Fecha
-//          </p>
-//          <span>{endDate}</span>
-//        </div>
-
-//        <div className={s.input}>
-//          <p>
-//            <FaClock style={{ color: "var(--bright-color)" }} /> Horario
-//          </p>
-//          <span>{time}</span>
-//        </div>
-
-//        <div className={s.input}>
-//          <p>
-//            <MdGroups style={{ color: "var(--bright-color)" }} /> Personas
-//          </p>
-//          <span> {pax}</span>
-//        </div>
-
-//      </div>
-//    </div>
-//  );
-//}
