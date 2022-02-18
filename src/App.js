c2ada46fc00196c71836f1fa512046eea3317ecc
erikamasini1 <91484157+erikamasini1@users.noreply.github.com>
2022-02-18 17:56:46 -0300

    merge

--- src/App.js
+++ src/App.js
@@ -32,9 +32,12 @@ function App() {
 
       <Route path={"/restaurants/:id"} render={() => <Details />} />
       <Route path={"/Login"} render={() => <Login />} />
-      <Route path={"/images"} render={() => <LoadImage />} />
+      <Route path={"/images"} render={() => <LoadImage />} /> 
+       {/* eslint-disable-next-line */}
       <Route exact path={"/reservations"} render={() => <Reservations />} />
+      {/* eslint-disable-next-line */}
       <Route exact path={"/success"} render={() => <MercadoPago />} />
+      {/* eslint-disable-next-line */}
       <Route exact path={"/canceled"} render={() => <PaymentError />} />
     </div>
   );
