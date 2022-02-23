f90b5dd13d001940e44d678fc37e727bbeafc1f2
Christian Hampel <80498051+chrishampel88@users.noreply.github.com>
2022-02-23 08:54:34 -0300

    linkedin

--- src/components/nosotros/Nosotros.jsx
+++ src/components/nosotros/Nosotros.jsx
@@ -17,14 +17,14 @@ export default function Nosotros(props) {
         <Modal.Body>
           <h4>Team Hero</h4>
           <div>
-            <a href="https://www.linkedin.com/in/erikaberenicemasini/" target="_blank" className={s.link}> Erika Masini</a><br></br>
-            <a href="https://www.linkedin.com/in/florenciaperezduarte/" target="_blank" className={s.link}> Florencia Pereze Duarte</a><br></br>
-            <a href="https://www.linkedin.com/in/sol-patrone/" target="_blank" className={s.link}> Sol Patrone</a><br></br>
-            <a href="https://www.linkedin.com/in/christian-hampel88/" target="_blank" className={s.link}> Christian Hampel</a><br></br>
-            <a href="https://www.linkedin.com/in/sergio-cepeda-dev/" target="_blank" className={s.link}> Sergio Cepeda</a><br></br>
-            <a href="https://www.linkedin.com/in/juanmurillop/" target="_blank" className={s.link}> Juan Carlos Murrillo </a><br></br>
-            <a href="https://www.linkedin.com/in/mprecelle/" target="_blank" className={s.link}> MartinPrecelle</a><br></br>
-            <a href="https://www.linkedin.com/in/josecossibracho/" target="_blank" className={s.link}> Jose Cossi</a><br></br>
+            <a href="https://www.linkedin.com/in/erikaberenicemasini/" target="_blank"  rel="noreferrer" className={s.link}> Erika Masini</a><br></br>
+            <a href="https://www.linkedin.com/in/florenciaperezduarte/" target="_blank"  rel="noreferrer" className={s.link}> Florencia Pereze Duarte</a><br></br>
+            <a href="https://www.linkedin.com/in/sol-patrone/" target="_blank"  rel="noreferrer" className={s.link}> Sol Patrone</a><br></br>
+            <a href="https://www.linkedin.com/in/christian-hampel88/" target="_blank"  rel="noreferrer" className={s.link}> Christian Hampel</a><br></br>
+            <a href="https://www.linkedin.com/in/sergio-cepeda-dev/" target="_blank"  rel="noreferrer" className={s.link}> Sergio Cepeda</a><br></br>
+            <a href="https://www.linkedin.com/in/juanmurillop/" target="_blank"  rel="noreferrer" className={s.link}> Juan Carlos Murrillo </a><br></br>
+            <a href="https://www.linkedin.com/in/mprecelle/" target="_blank"  rel="noreferrer" className={s.link}> MartinPrecelle</a><br></br>
+            <a href="https://www.linkedin.com/in/josecossibracho/" target="_blank"  rel="noreferrer" className={s.link}> Jose Cossi</a><br></br>
             </div>
         </Modal.Body>
         <Modal.Footer>
